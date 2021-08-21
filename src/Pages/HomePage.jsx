9a7cfb7b7879207363bc6409da0f419c37deab8b
rudrakshi99 <sonirudrakshi99@gmail.com>
2021-08-21 10:09:48 +0530

    minor fixes

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -12,24 +12,24 @@ const HomePage = () => {
             <ImageSection />
             <header className="hero">
                 <div className="icons">
-                    <Link to="https://www.linkedin.com/in/rudrakshi-soni-403031195/" className="icon-holder">
+                    <a href="https://www.linkedin.com/in/rudrakshi-soni-403031195/" className="icon-holder">
                         <FontAwesomeIcon icon={faLinkedin} className="icon fb" />
-                    </Link>
-                    <Link to="https://github.com/rudrakshi99" className="icon-holder">
+                    </a>
+                    <a href="https://github.com/rudrakshi99" className="icon-holder">
                         <FontAwesomeIcon icon={faGithub} className="icon gh" />
-                    </Link>
-                    <Link to="https://twitter.com/Rudrakshi09" className="icon-holder">
+                    </a>
+                    <a href="https://twitter.com/Rudrakshi09" className="icon-holder">
                         <FontAwesomeIcon icon={faTwitter} className="icon tw"/>
-                    </Link>
-                    <Link to="https://medium.com/@sonirudrakshi99" className="icon-holder">
+                    </a>
+                    <a href="https://medium.com/@sonirudrakshi99" className="icon-holder">
                         <FontAwesomeIcon icon={faMedium} className="icon md"/>
-                    </Link>
-                    <Link to="https://dev.to/rudrakshi99" className="icon-holder">
+                    </a>
+                    <a href="https://dev.to/rudrakshi99" className="icon-holder">
                         <FontAwesomeIcon icon={faDev} className="icon dev"/>
-                    </Link>
-                    <Link to="https://stackoverflow.com/users/12773738/rudrakshi?tab=profile" className="icon-holder">
+                    </a>
+                    <a href="https://stackoverflow.com/users/12773738/rudrakshi?tab=profile" className="icon-holder">
                         <FontAwesomeIcon icon={faStackOverflow} className="icon sk"/>
-                    </Link>
+                    </a>
                 </div>
             </header>
         </div>
