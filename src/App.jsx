959e9897de9b1e45223d3eb1b4318205a3092d43
rudrakshi99 <sonirudrakshi99@gmail.com>
2022-01-22 22:48:21 +0530

    feat: added defualt redirection to endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
-import './App.scss';
-import NavBar from './Components/NavBar';
-import HomePage from './Pages/HomePage';
-import {Switch, Route} from 'react-router-dom';
-import SkillsPage from './Pages/SkillsPage';
-import PortfliosPage from './Pages/PortfoliosPage';
-import BlogsPage from './Pages/BlogsPage';
-import ContactPage from './Pages/ContactPage';
-import { useState } from 'react';
-
-
+import "./App.scss";
+import NavBar from "./Components/NavBar";
+import HomePage from "./Pages/HomePage";
+import { Switch, Route } from "react-router-dom";
+import SkillsPage from "./Pages/SkillsPage";
+import PortfliosPage from "./Pages/PortfoliosPage";
+import BlogsPage from "./Pages/BlogsPage";
+import ContactPage from "./Pages/ContactPage";
+import { useState } from "react";
 
 export const App = () => {
   const [navToggle, setNavToggle] = useState(false);
 
-  const navClick = () =>{
-    setNavToggle(!navToggle)
-  }
+  const navClick = () => {
+    setNavToggle(!navToggle);
+  };
 
   return (
     <div className="App">
-      <div className={`sidebar ${navToggle ? 'nav-toggle': ''}`}>
+      <div className={`sidebar ${navToggle ? "nav-toggle" : ""}`}>
         <NavBar />
       </div>
       <div className="nav-btn" onClick={navClick}>
@@ -28,28 +26,29 @@ export const App = () => {
         <div className="lines-3"></div>
       </div>
       <div className="main-content">
-          <div className="content">
-            <Switch>
-              <Route path="/" exact>
-                <HomePage />
-              </Route>
-              <Route path="/skills" exact>
-                <SkillsPage />
-              </Route>
-              <Route path="/projects" exact>
-                <PortfliosPage />
-              </Route>
-              <Route path="/blogs" exact>
-                <BlogsPage />
-              </Route>
-              <Route path="/contact" exact>
-                <ContactPage />
-              </Route>
-            </Switch>
-          </div>
+        <div className="content">
+          <Switch>
+            <Route path="/" exact>
+              <HomePage />
+            </Route>
+            <Route path="/skills" exact>
+              <SkillsPage />
+            </Route>
+            <Route path="/projects" exact>
+              <PortfliosPage />
+            </Route>
+            <Route path="/blogs" exact>
+              <BlogsPage />
+            </Route>
+            <Route path="/contact" exact>
+              <ContactPage />
+            </Route>
+            <Route path="*" exact>
+              <HomePage />
+            </Route>
+          </Switch>
+        </div>
       </div>
     </div>
   );
-}
-
-
+};
