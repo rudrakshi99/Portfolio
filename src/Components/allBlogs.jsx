a63ef9e0488b535589c8b1611c7b96fab578e542
rudrakshi99 <sonirudrakshi99@gmail.com>
2022-01-22 22:38:32 +0530

    feat: added new blog

--- src/Components/allBlogs.jsx
+++ src/Components/allBlogs.jsx
@@ -1,39 +1,45 @@
-import blog1 from '../img/blogs/blog1.jpeg';
-import blog2 from '../img/blogs/blog2.jpeg';
-import blog3 from '../img/blogs/blog3.jpeg';
-
+import blog1 from "../img/blogs/blog1.jpeg";
+import blog2 from "../img/blogs/blog2.jpeg";
+import blog3 from "../img/blogs/blog3.jpeg";
+import blog4 from "../img/blogs/blog4.png";
 
 const blogs = [
-    {
-        id: 1,
-        image: blog1,
-        title: 'Linux Basics',
-        link: 'https://dev.to/rudrakshi99/linux-basics-2onj',
-        date : '12',
-        month: 'Jan',
-        year: '2021'
-    },
-    {
-        id: 2,
-        image: blog3,
-        title: 'Crio Winter of Doing: Externships',
-        link: 'https://dev.to/rudrakshi99/crio-winter-of-doing-externships-17gi',
-        date : '19',
-        month: 'Apr',
-        year: '2021'
-    },
-    {
-        id: 3,
-        image: blog2,
-        title: 'GSSoC 2020: A journey of 92 Days',
-        link: 'https://medium.com/girlscript-summer-of-code/gssoc-2020-a-journey-of-92-days-56a32e6000f4',
-        date : '01',
-        month: 'June',
-        year: '2020'
-    },
-    
-    
-    
+  {
+    id: 1,
+    image: blog1,
+    title: "Linux Basics",
+    link: "https://dev.to/rudrakshi99/linux-basics-2onj",
+    date: "12",
+    month: "Jan",
+    year: "2021",
+  },
+  {
+    id: 2,
+    image: blog3,
+    title: "Crio Winter of Doing: Externships",
+    link: "https://dev.to/rudrakshi99/crio-winter-of-doing-externships-17gi",
+    date: "19",
+    month: "Apr",
+    year: "2021",
+  },
+  {
+    id: 3,
+    image: blog2,
+    title: "GSSoC 2020: A journey of 92 Days",
+    link: "https://medium.com/girlscript-summer-of-code/gssoc-2020-a-journey-of-92-days-56a32e6000f4",
+    date: "01",
+    month: "June",
+    year: "2020",
+  },
+  {
+    id: 4,
+    image: blog4,
+    title: "Bash Shell Scripting Guide",
+    link: "https://rudrakshi.hashnode.dev/bash-shell-scripting-guide",
+    date: "01",
+    month: "Jan",
+    year: "2022",
+  },
 ];
 
 export default blogs;
