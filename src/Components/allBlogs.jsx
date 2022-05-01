0cdfe95d992028433143d8d9e14b87df8aafb664
rudrakshi99 <sonirudrakshi99@gmail.com>
2022-05-01 16:23:04 +0000

    added blogs

--- src/Components/allBlogs.jsx
+++ src/Components/allBlogs.jsx
@@ -2,6 +2,8 @@ import blog1 from "../img/blogs/blog1.jpeg";
 import blog2 from "../img/blogs/blog2.jpeg";
 import blog3 from "../img/blogs/blog3.jpeg";
 import blog4 from "../img/blogs/blog4.png";
+import blog5 from "../img/blogs/blog5.png";
+import blog6 from "../img/blogs/blog6.jpeg";
 
 const blogs = [
   {
@@ -38,6 +40,24 @@ const blogs = [
     link: "https://rudrakshi.hashnode.dev/bash-shell-scripting-guide",
     date: "01",
     month: "Jan",
+    year: "2022",
+  },
+  {
+    id: 5,
+    image: blog5,
+    title: "begin() and end() in Array C++ STL",
+    link: "https://iq.opengenus.org/begin-and-end-array-cpp/",
+    date: "15",
+    month: "May",
+    year: "2020",
+  },
+  {
+    id: 6,
+    image: blog6,
+    title: "Morgan Stanley Interview Experience for Technology Summer Analyst 2022 Internship (Off-Campus)",
+    link: "https://medium.com/@sonirudrakshi99/morgan-stanley-interview-experience-for-technology-summer-analyst-2021-internship-off-campus-bb1d179213a7",
+    date: "30",
+    month: "March",
     year: "2022",
   },
 ];
